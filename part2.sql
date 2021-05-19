acfddabc6a0aece8dfd93f363c1b9a74855c69f3
Sana Mohiuddin <smohi002@ucr.edu>
2021-05-19 01:16:20 -0700

    Added drop statements

---
+++ part2.sql
@@ -0,0 +1,16 @@
+--
+-- Database Table Creation
+--
+-- This file will create the tables for the diagram given in part 1
+--
+-- First drop any existing tables. Any errors are ignored.
+--
+DROP TABLE hospital
+DROP TABLE department
+DROP TABLE doctor
+DROP TABLE patient
+DROP TABLE appointment
+DROP TABLE staff
+
+
+
