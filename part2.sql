753ad17beeae52c82e2da8a50e3dd1cd9b984c61
Sana Mohiuddin <smohi002@wch135-09.cs.ucr.edu>
2021-05-19 11:19:59 -0700

    Added not null to keys

--- part2.sql
+++ part2.sql
@@ -20,41 +20,41 @@ DROP TABLE if EXISTS schedule CASCADE;
 DROP TABLE if EXISTS request_maintenance CASCADE;
 DROP TABLE if EXISTS searches CASCADE;
 
-CREATE TABLE hospital (hid INTEGER,
+CREATE TABLE hospital (hid INTEGER NOT NULL,
                        hname CHAR(40),
                        PRIMARY KEY(hid));
 
-CREATE TABLE department_includes (did INTEGER,
-                                  hid INTEGER,
+CREATE TABLE department_includes (did INTEGER NOT NULL,
+                                  hid INTEGER NOT NULL,
                                   dname CHAR(40),
                                   PRIMARY KEY(did),
                                   FOREIGN KEY (hid) REFERENCES hospital -- many depts in a hospital
                                   ON DELETE CASCADE);                     -- participation constraint
                                  
-CREATE TABLE doctor_worksdept (docid INTEGER,
-                               did INTEGER,
+CREATE TABLE doctor_worksdept (docid INTEGER NOT NULL,
+                               did INTEGER NOT NULL,
                                docname CHAR(40),
                                specialty CHAR(40),
                                PRIMARY KEY(docid),
                                FOREIGN KEY (did) REFERENCES department_includes -- many docs in a dept
                                ON DELETE CASCADE);                                -- participation constraint);
 
-CREATE TABLE patient (pid INTEGER,
-                     pname CHAR(40),
-                     gender CHAR(40),
-                     age INTEGER,
-                     address CHAR(40),
-                     total_appt INTEGER,
-                     PRIMARY KEY(pid));
+CREATE TABLE patient (pid INTEGER NOT NULL,
+                      pname CHAR(40),
+                      gender CHAR(40),
+                      age INTEGER,
+                      address CHAR(40),
+                      total_appt INTEGER,
+                      PRIMARY KEY(pid));
 
 
-CREATE TABLE appt (aid INTEGER,
-                          date DATE,
-                          time_slot CHAR(10),
-                          PRIMARY KEY(aid));
+CREATE TABLE appt (aid INTEGER NOT NULL,
+                   date DATE,
+                   time_slot CHAR(10),
+                   PRIMARY KEY(aid));
                           
-CREATE TABLE staff_worksin (sid INTEGER,
-                            aid INTEGER,
+CREATE TABLE staff_worksin (sid INTEGER NOT NULL,
+                            aid INTEGER NOT NULL,
                             sname CHAR(40),
                             PRIMARY KEY(sid),
                             FOREIGN KEY (aid) REFERENCES appt -- many staff work in a hospital
@@ -62,43 +62,43 @@ CREATE TABLE staff_worksin (sid INTEGER,
 
 -- ISA Heirarchy
 
-CREATE TABLE past_appt (aid INTEGER,
+CREATE TABLE past_appt (aid INTEGER NOT NULL,
                         PRIMARY KEY(aid),
                         FOREIGN KEY (aid) REFERENCES appt
                         ON DELETE CASCADE);
 
-CREATE TABLE waitlisted_appt (aid INTEGER,
+CREATE TABLE waitlisted_appt (aid INTEGER NOT NULL,
                               PRIMARY KEY(aid),
                               FOREIGN KEY (aid) REFERENCES appt
                               ON DELETE CASCADE);
                         
-CREATE TABLE active_appt (aid INTEGER,
+CREATE TABLE active_appt (aid INTEGER NOT NULL,
                           PRIMARY KEY(aid),
                           FOREIGN KEY (aid) REFERENCES appt
                           ON DELETE CASCADE);
 
-CREATE TABLE avail_appt (aid INTEGER,
+CREATE TABLE avail_appt (aid INTEGER NOT NULL,
                          PRIMARY KEY(aid),
                          FOREIGN KEY (aid) REFERENCES appt
                          ON DELETE CASCADE);
 
 
 -- Relationship Tables
-CREATE TABLE has (aid INTEGER,
-                  docid INTEGER,
+CREATE TABLE has (aid INTEGER NOT NULL,
+                  docid INTEGER NOT NULL,
                   PRIMARY KEY(aid, docid),
                   FOREIGN KEY (aid) REFERENCES appt,
                   FOREIGN KEY (docid) REFERENCES doctor_worksdept); -- many docs can attend an appt and a doc can have many appts
                   
-CREATE TABLE schedule (aid INTEGER,
-                       sid INTEGER,
+CREATE TABLE schedule (aid INTEGER NOT NULL,
+                       sid INTEGER NOT NULL,
                        PRIMARY KEY(aid, sid),
                        FOREIGN KEY (aid) REFERENCES appt,
                        FOREIGN KEY (sid) REFERENCES staff_worksin); --staff can schedule many appts and different staff can edit the same appt
                        
-CREATE TABLE request_maintenance (sid INTEGER,
-                                  docid INTEGER,
-                                  patient_per_hr INTEGER,
+CREATE TABLE request_maintenance (sid INTEGER NOT NULL,
+                                  docid INTEGER NOT NULL,
+                                  patient_per_hr INTEGER NOT NULL,
                                   dept_name CHAR(40),
                                   time_slot CHAR(40),
                                   PRIMARY KEY(sid, docid),
